46e9601a3f0e56f86682e6cb8357547c70012282
rebuilding-patrick <51345183+rebuilding-patrick@users.noreply.github.com>
2019-07-15 18:07:45 -0700

    Add files via upload
    Server now takes care of its own tick for keepalives without driving game logic. Moved message handling to the game to avoid handling each message twice. MessagePools created to avoid garbage collection for the potentially hundreds of messages being passed a second. Some other design stuff for greater efficiency and cleaner design.

--- server.py
+++ server.py
@@ -1,38 +1,11 @@
 import socket
-import time
-
+from sharing import *
 from threading import Thread
-from sharing import Message, Clock
 
-up = 1 << 0
-down = 1 << 1
-left = 1 << 2
-right = 1 << 3
-shift = 1 << 4
-ctrl = 1 << 5
 
-accept = 1 << 0
-select = 1 << 1
-r = 1 << 2
-f = 1 << 3
-q = 1 << 4
-e = 1 << 5
-z = 1 << 6
-tab = 1 << 7
-esc = 1 << 8
-
-PING_RATE = 100
+PING_RATE = 10
 CONNECTION_WARN_RATE = 50
 TIMEOUT_WARNINGS_TO_KILL = 3
-
-GET_WORLD_COMMAND = -1
-JOIN_COMMAND = 0
-UPDATE_PLAYER_COMMAND = 1
-UPDATE_PLAYERS_COMMAND = 2
-PLAYER_INPUT_COMMAND = 3
-GET_ACTORS_COMMAND = 4
-GET_ACTOR_COMMAND = 5
-MESSAGE_COMMAND = 6
 
 
 class Server(Thread):
@@ -48,6 +21,7 @@ class Server(Thread):
         self.game = game
         self.input_channel = game.input_channel
         self.output_channel = game.output_channel
+        self.message_pool = MessagePool(15*8*3)  # 15msg a second in, 8 clients, 3 second buffer
 
         self.connections = ConnectionManager(self)
         self.connections.start()
@@ -85,71 +59,8 @@ class Server(Thread):
         command = int(data[1])
         value = data[2]
 
-        message = Message(index, command, value, host)
-        message.raw_data = raw_data
-
-        if command == PLAYER_INPUT_COMMAND:
-            value = value.split(':')
-            directions = int(value[0])
-            buttons = int(value[1])
-            inputs = {}
-
-            if directions & up:
-                inputs['up'] = True
-            if directions & down:
-                inputs['down'] = True
-            if directions & left:
-                inputs['left'] = True
-            if directions & right:
-                inputs['right'] = True
-            if directions & shift:
-                inputs['shift'] = True
-            if directions & ctrl:
-                inputs['ctrl'] = True
-
-            if buttons & accept:
-                inputs['accept'] = True
-            if buttons & select:
-                inputs['select'] = True
-            if buttons & r:
-                inputs['r'] = True
-            if buttons & f:
-                inputs['f'] = True
-            if buttons & q:
-                inputs['q'] = True
-            if buttons & e:
-                inputs['e'] = True
-            if buttons & z:
-                inputs['z'] = True
-            if buttons & tab:
-                inputs['tab'] = True
-            if buttons & esc:
-                inputs['esc'] = True
-
-            message.inputs = inputs
-
-            #if inputs:
-            #    print('Server: %s recv from %s at %s' % (inputs, message.host_name, message.index))
-
-        elif command == GET_ACTORS_COMMAND:
-            pass
-
-        elif command == GET_ACTOR_COMMAND:
-            pass
-
-        elif command == MESSAGE_COMMAND:
-            pass
-
-        elif command == JOIN_COMMAND:
-            value = value.split(':|:')
-            message.username = value[0]
-            message.password = value[1]
-
-            print('Server: Join from %s at %s' % (message.username, message.host_name))
-
-        elif command == GET_WORLD_COMMAND:
-
-            print('Server: World request from %s' % message.host_name)
+        message = self.message_pool.get(index, command, value, host)
+        message.values['raw_data'] = raw_data
 
         return message
 
@@ -161,7 +72,8 @@ class ConnectionManager(Thread):
         self.output_channel = server.output_channel
         self.host = server.host
 
-        self.clock = Clock(1/15.0)
+        self.message_pool = MessagePool(15 * 8 * 3)  # 15msg/sec, 8 clients, 3 second buffer
+        self.clock = Clock(PING_RATE)
         self.connections = {}
 
 
@@ -169,18 +81,13 @@ class ConnectionManager(Thread):
         while True:
             #try:
             if self.clock.tick():
-                message = Message(self.clock.value, UPDATE_PLAYERS_COMMAND, 'new_turn', self.host)
-                self.input_channel.give(message, 'game')
-
-                if self.clock.value % PING_RATE == 0:
-                    self.check_connections()
+                self.check_connections()
 
             connections = self.connections
             for host in connections:
-                    if self.output_channel.has(host):
-                        messages = self.output_channel.get(host)
-                        for message in messages:
-                            connections[host].send(message, self.clock.value)
+                messages = self.output_channel.get(host)
+                for message in messages:
+                    connections[host].send(message)
 
             #except:
             #    print('Connection Error')
@@ -206,7 +113,6 @@ class ConnectionManager(Thread):
                     healthy_connections[host] = connection
                 else:
                     print('Server: Disconnect from %s:%s' % host)
-
 
         self.connections = healthy_connections
 
@@ -247,8 +153,8 @@ class Connection:
     def handshake(self):
         return
 
-    def send(self, message, tick):
-        packet = message.get_packet(tick)
+    def send(self, message):
+        packet = message.get_packet()
         self.send_data(packet)
 
     def send_data(self, data):
--- sharing.py
+++ sharing.py
@@ -2,6 +2,38 @@ from threading import Lock
 import time
 import numpy
 
+GET_WORLD_COMMAND = -1
+JOIN_COMMAND = 0
+UPDATE_PLAYER_COMMAND = 1
+UPDATE_PLAYERS_COMMAND = 2
+PLAYER_INPUT_COMMAND = 3
+ACTOR_INPUT_COMMAND = 4
+GET_ACTORS_COMMAND = 5
+GET_ACTOR_COMMAND = 6
+MESSAGE_COMMAND = 7
+ADD_ACTOR_COMMAND = 8
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+up_key = 1 << 0
+down_key = 1 << 1
+left_key = 1 << 2
+right_key = 1 << 3
+shift_key = 1 << 4
+ctrl_key = 1 << 5
+
+accept_key = 1 << 0
+select_key = 1 << 1
+r_key = 1 << 2
+f_key = 1 << 3
+q_key = 1 << 4
+e_key = 1 << 5
+z_key = 1 << 6
+tab_key = 1 << 7
+esc_key = 1 << 8
 
 class Clock:
     def __init__(self, rate):
@@ -39,7 +71,10 @@ class SharedList:
 
     def get(self, address):
         self.lock.acquire()
-        items = self.items[address]
+        if address in self.items:
+            items = self.items[address]
+        else:
+            items = []
         self.items[address] = []
         self.lock.release()
         return items
@@ -48,15 +83,35 @@ class SharedList:
         return Message(index, command, value, host)
 
 
+class MessagePool:
+    def __init__(self, size):
+        self.size = size
+        self.index = 0
+        self.pool = []
+        for i in range(size):
+            self.pool.append(Message(0, 0, 0, (0, 0)))
+
+    def get(self, index, command, value, host):
+        message = self.pool[self.index]
+        message.update(index, command, value, host)
+        self.index += 1
+        self.index = self.index % self.size
+        return message
+
+
 class Message:
     def __init__(self, index, command, value, host):
         self.index = index
         self.command = command
         self.value = value
+        self.values = {}
         self.host = host
         self.host_name = '%s:%s' % (host[0], host[1])
 
-    def get_packet(self, tick):
-        packet = '%s][%s][%s' % (tick, self.command, self.value)
+    def update(self, index, command, value, host):
+        self.__init__(index, command, value, host)
+
+    def get_packet(self):
+        packet = '%s][%s][%s' % (self.index, self.command, self.value)
         return packet.encode()
 
